7c7322602bcbd0f684798995ffdfd8e71caf06d5
jdc2000 <jdc2000@live.com>
2020-02-22 01:23:22 -0600

    Add cloning and class name variants

--- packages/icon/index.js
+++ packages/icon/index.js
@@ -3,17 +3,27 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 function Icon({ className, icon, ...otherProps }) {
-  const classes = classNames('fat-icon', 'material-icons', className);
-  return (
-    <i className={classes} {...otherProps}>
-      {icon}
-    </i>
-  );
+  const material = typeof icon === 'string';
+  if (!icon || material) {
+    const classes = classNames(
+      'fat-icon',
+      { 'material-icons': material },
+      className
+    );
+    return (
+      <i className={classes} {...otherProps}>
+        {icon}
+      </i>
+    );
+  }
+  const classes = classNames('fat-icon', icon.props.className, className);
+  const props = { ...otherProps, ...icon.props, className: classes };
+  return React.cloneElement(icon, props);
 }
 
 Icon.propTypes = {
   className: PropTypes.string,
-  icon: PropTypes.string.isRequired
+  icon: PropTypes.oneOfType([PropTypes.string, PropTypes.element])
 };
 
 export { Icon };
--- packages/icon/stories/Icon.stories.js
+++ packages/icon/stories/Icon.stories.js
@@ -3,4 +3,10 @@ import { Icon } from '..';
 
 export default { title: 'Icon' };
 
-export const Basic = () => <Icon icon="favorite" />;
+export const Basic = () => <Icon className="fas fa-heart" />;
+
+export const Material = () => <Icon icon="favorite" />;
+
+export const WithIconElement = () => (
+  <Icon icon={<i className="fas fa-ice-cream fa-2x"></i>} />
+);

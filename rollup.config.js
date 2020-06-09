8bd2d0a5c72412ae233fd7ebe889cc8f0e2987d2
jdc2000 <jdc2000@live.com>
2020-06-09 02:48:52 -0500

    Add mdc min css to dist directory

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,44 @@
 import babel from 'rollup-plugin-babel';
+import copy from 'rollup-plugin-copy';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import fs from 'fs';
 import path from 'path';
 
 const PACKAGES = 'packages';
+function getCssPath(pkg) {
+  return path.resolve(`node_modules/@material/${pkg}/dist/mdc.${pkg}.min.css`);
+}
+function getDistPath(filepath) {
+  return path.resolve(filepath, 'dist');
+}
+function getExternal() {
+  return ['react', 'prop-types', 'classnames'];
+}
+function getPlugins() {
+  return [
+    resolve(),
+    babel({
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            useBuiltIns: 'entry',
+            corejs: 3,
+            modules: false,
+            exclude: ['transform-typeof-symbol']
+          }
+        ],
+        ['@babel/preset-react', { development: false, useBuiltIns: true }]
+      ],
+      plugins: [
+        ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }]
+      ]
+    }),
+    terser()
+  ];
+}
+
 export default fs
   .readdirSync(PACKAGES)
   .map(pkg => {
@@ -13,7 +47,8 @@ export default fs
     if (!stat.isDirectory()) {
       return null;
     }
-    const external = ['react', 'prop-types', 'classnames'];
+    const external = getExternal();
+    const plugins = getPlugins();
     if (pkg === 'menu-surface') {
       external.push('react-dom');
     }
@@ -26,6 +61,27 @@ export default fs
     if (pkg === 'typeahead') {
       external.push('fuse.js', 'react-dom', 'uuid');
     }
+    if (pkg === 'tabs') {
+      plugins.push(
+        copy({
+          targets: [
+            { src: getCssPath('tab'), dest: getDistPath(filepath) },
+            { src: getCssPath('tab-bar'), dest: getDistPath(filepath) },
+            { src: getCssPath('tab-indicator'), dest: getDistPath(filepath) },
+            { src: getCssPath('tab-scroller'), dest: getDistPath(filepath) }
+          ],
+          verbose: true
+        })
+      );
+    }
+    if (pkg !== 'icon' && pkg !== 'tabs' && pkg !== 'typeahead') {
+      plugins.push(
+        copy({
+          targets: [{ src: getCssPath(pkg), dest: getDistPath(filepath) }],
+          verbose: true
+        })
+      );
+    }
     return {
       external,
       input: path.resolve(filepath, 'index.js'),
@@ -35,27 +91,7 @@ export default fs
           format: 'esm'
         }
       ],
-      plugins: [
-        resolve(),
-        babel({
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                useBuiltIns: 'entry',
-                corejs: 3,
-                modules: false,
-                exclude: ['transform-typeof-symbol']
-              }
-            ],
-            ['@babel/preset-react', { development: false, useBuiltIns: true }]
-          ],
-          plugins: [
-            ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }]
-          ]
-        }),
-        terser()
-      ]
+      plugins
     };
   })
   .filter(opt => opt);

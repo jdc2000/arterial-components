a68a4b8da40f49f6c8c681118294dd33864df7cb
jdc2000 <jdc2000@live.com>
2020-12-05 03:10:28 -0600

    Update rollup config
    Update prettier config

--- rollup.config.js
+++ rollup.config.js
@@ -4,28 +4,20 @@ import fs from 'fs';
 import path from 'path';
 
 const PACKAGES = 'packages';
-export default fs
-  .readdirSync(PACKAGES)
-  .map((pkg) => {
+async function getConfig() {
+  const result = [];
+  const packages = fs.readdirSync(PACKAGES);
+  for (const pkg of packages) {
     const filepath = path.resolve(PACKAGES, pkg);
     const stat = fs.statSync(filepath);
-    if (!stat.isDirectory()) {
-      return null;
-    }
-    const external = ['react', 'prop-types', 'classnames'];
-    const pkgJson = fs.readFileSync(`${filepath}/package.json`);
-    const { dependencies } = JSON.parse(pkgJson);
-    const keys = dependencies ? Object.keys(dependencies) : [];
-    for (const key of keys) external.push(key);
-    return {
+    if (!stat.isDirectory() || pkg === 'date-picker') continue;
+    const {dependencies} = await import(`${filepath}/package.json`);
+    const deps = Object.keys(dependencies || {});
+    const external = ['react', 'prop-types', 'classnames', ...deps];
+    result.push({
       external,
       input: path.resolve(filepath, 'index.js'),
-      output: [
-        {
-          file: path.resolve(filepath, 'dist/index.js'),
-          format: 'esm',
-        },
-      ],
+      output: [{file: path.resolve(filepath, 'dist/index.js'), format: 'cjs'}],
       plugins: [
         resolve(),
         babel({
@@ -40,16 +32,16 @@ export default fs
                 exclude: ['transform-typeof-symbol'],
               },
             ],
-            ['@babel/preset-react', { development: false, useBuiltIns: true }],
+            ['@babel/preset-react', {development: false, useBuiltIns: true}],
           ],
           plugins: [
-            [
-              '@babel/plugin-proposal-object-rest-spread',
-              { useBuiltIns: true },
-            ],
+            ['@babel/plugin-proposal-object-rest-spread', {useBuiltIns: true}],
           ],
         }),
       ],
-    };
-  })
-  .filter((opt) => opt);
+    });
+  }
+  return result;
+}
+export const config = getConfig();
+export default config;
